74b8d3a7f59caa5f9eee725bbd4ca7551fb1b564
Anil Kumar <93667898+anil29717@users.noreply.github.com>
2025-02-13 22:10:53 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { CartProvider } from "./context/CartContext";
-import ProductList from "./pages/ProductList";
+import { CartProvider } from "./context/cartContext";
+import ProductList from "./pages/productList";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/CheckOut";
 import Navbar from "./component/Navbar";
